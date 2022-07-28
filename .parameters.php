7b8724e2fa6c201dd59dec30beb41e76a859998d
Egor <gosha31rus.14@gmail.com>
2022-07-28 16:41:03 +0300

    update .parameters.php

--- .parameters.php
+++ .parameters.php
@@ -1,7 +1,21 @@
 <? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {die();} ?>
-<?CModule::IncludeModule("iblock")?>
 
 <?
+
+  function createTextField ($name = "Имя поля", $default = '', $parent = 'BASE') {
+    return [
+      'NAME' => $name, 'TYPE' => 'STRING',
+      'DEFAULT' => $default, 'PARENT' => $parent,
+    ];
+  }
+
+  function createListField ($name = "Имя поля", $values = '', $parent = 'BASE') {
+    return [
+      'NAME' => $name, 'TYPE' => 'LIST',
+      'VALUES' => $values, 'PARENT' => $parent,
+    ];
+  }
+
   $fileTypeParams = [
     "image" => "картинка",
     "file"  => "HTML файл",
@@ -10,43 +24,16 @@
   $arComponentParameters = [
     "GROUPS" => [
       "TEMPLATE" => ["NAME" => "Параметры шаблона"],
-      "LOGIC" => ["NAME" => "Очередь показа картинок"],
+      "LOGIC"    => ["NAME" => "Очередь показа картинок"],
     ],
 
     "PARAMETERS" => [
-      "CONTENT_TYPE" => [
-        "NAME" => "Тип контента",
-        "TYPE" => "LIST",
-        "VALUES" => $fileTypeParams,
-      ],
-
-      "IMG_PATH" => [
-        "NAME" => "Путь к артинке",
-        "TYPE" => "STRING",
-      ],
-
-      "HTML_PATH" => [
-        "NAME" => "Путь к html файлу",
-        "TYPE" => "STRING",
-      ],
-
-      "LINK_ON_POPUP" => [
-        "NAME" => "Ссылка на картинке",
-        "TYPE" => "STRING",
-        "DEFAULT" => "#",
-      ],
-
-      "SHOW_DELAY" => [
-        "NAME" => "Задержка перед показом в секундах",
-        "TYPE" => "STRING",
-        "DEFAULT" => "1",
-      ],
-
-      "SHOW_TIME" => [
-        "NAME" => "Время показа в секундах (0 - автозакрытия нету)",
-        "TYPE" => "STRING",
-        "DEFAULT" => "5",
-      ],
+      "CONTENT_TYPE"  => createListField('Тип контента', $fileTypeParams),
+      "IMG_PATH"      => createTextField('Путь к картинке'),
+      "HTML_PATH"     => createTextField('Путь к файлу'),
+      "LINK_ON_POPUP" => createTextField('Ссылка на фрейме', '#'),
+      "SHOW_DELAY"    => createTextField('Задержка показа (сек)', '1'),
+      "SHOW_TIME"     => createTextField('Время показа (0 - постоянно)', '10'),
     ],
   ];
 
